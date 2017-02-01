4bb42ca83f2a215ef48e0f2fee44f4c2fa407756
Ceki Gulcu <ceki@qos.ch>
2017-02-01 17:07:33 +0100

    ow

--- src/main/java/ch/qos/logback/FileAppenderBenchmark.java
+++ src/main/java/ch/qos/logback/FileAppenderBenchmark.java
@@ -55,7 +55,7 @@ public class FileAppenderBenchmark {
     }
 
     private void deleteLogFiles() {
-        final File logbackFile = new File("/tmp/testlogback.log");
+        final File logbackFile = new File("target/testlogback.log");
         logbackFile.delete();
         final File log4jRandomFile = new File("target/testRandomlog4j2.log");
         log4jRandomFile.delete();
