9e8aebd3849fa062454c62ebb71c46c01adf0802
Ceki Gulcu <ceki@qos.ch>
2017-02-05 11:31:27 +0100

    cosmetics

--- runBenchmak.sh
+++ runBenchmak.sh
@@ -1,11 +1,27 @@
 
 
-rm /tmp/target/test*.log;
+# by default test ALL methods in FileAppenderBenchmar
+METHOD=log 
 
-for numThreads in 1 2; # 4 8 16 32 64;
+if [ ! -z $1 ]
+then
+  METHOD=$1
+   echo "Will run benchmark methods in FileAppenderBenchmark with names starting with [$METHOD]"
+else
+  echo "Will run all benchmark methods in FileAppenderBenchmark"
+fi
+
+echo "Results will be output into file $METHOD.csv"
+sleep 1
+
+
+for numThreads in 1 2 4 8 16 32 64;
 do
+    rm /tmp/target/test*.log;
     echo Number of threads: $i
-    $JAVA_HOME/bin/java -jar target/benchmarks.jar ".*FileAppenderBenchmark.*" -f 1  -tu ms -wi 5 -i 5 -t $numThreads -rff tmp.csv -rf csv
-    cat tmp.csv >> results.csv
+    $JAVA_HOME/bin/java -jar target/benchmarks.jar ".*FileAppenderBenchmark.${METHOD}" -f 1  -tu ms -wi 5 -i 10 -t $numThreads -rff tmp.csv -rf csv
+    cat tmp.csv >> $METHOD.csv
+    rm tmp.csv
 done
+
 
