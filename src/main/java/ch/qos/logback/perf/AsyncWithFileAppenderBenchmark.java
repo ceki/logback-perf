5ef0332f1c913e4fb888b34e8ce9cea04530b589
Ceki Gulcu <ceki@qos.ch>
2021-08-19 23:01:20 +0200

    ow
    Signed-off-by: Ceki Gulcu <ceki@qos.ch>

--- src/main/java/ch/qos/logback/perf/AsyncWithFileAppenderBenchmark.java
+++ src/main/java/ch/qos/logback/perf/AsyncWithFileAppenderBenchmark.java
@@ -33,50 +33,17 @@ import org.slf4j.LoggerFactory;
 
 
 /**
- * Benchmarks Log4j 2, Log4j 1, Logback and JUL using the DEBUG level which is
- * enabled for this test. The configuration for each uses a FileAppender
- */
+ * Benchmarks Logback AsyncAppender in conjunction with FileAppender */
+
 // HOW TO RUN THIS TEST
-// java -jar logback-perf/target/benchmarks.jar ".*AsyncWithFileAppenderBenchmark.*" -f 1
-// -wi 10 -i 20
-//
-// RUNNING THIS TEST WITH 4 THREADS:
-// java -jar logback-perf/target/benchmarks.jar ".*AsyncWithFileAppenderBenchmark.*" -f 1
-// -wi 10 -i 20 -t 4
+// java -jar  target/benchmarks.jar ".*AsyncWithFileAppenderBenchmark" -f 1  -tu ms -wi 2 -i 6 -t 32
 
-//# Run progress: 0.00% complete, ETA 00:01:00
-//# Fork: 1 of 1
-//# Warmup Iteration   1: 319.006 ops/ms
-//# Warmup Iteration   2: 299.692 ops/ms
-//Iteration   1: 156.006 ops/ms
-//Iteration   2: 246.079 ops/ms
-//Iteration   3: 261.095 ops/ms
-//Iteration   4: 282.310 ops/ms
-//
-//
-//Result "ch.qos.logback.perf.AsyncWithFileAppenderBenchmark.logbackFile":
-//  236.372 ±(99.9%) 359.294 ops/ms [Average]
-//  (min, avg, max) = (156.006, 236.372, 282.310), stdev = 55.601
-//  CI (99.9%): [≈ 0, 595.667] (assumes normal distribution)
+// Results with 1.3.0-alpha9 
+// 147.698 ±(99.9%) 2.416 ops/ms [Average]	
 
-// ================================================================
-// ================================= After the drainTo optimization
-
-//Iteration   1: 391.968 ops/ms
-//Iteration   2: 732.784 ops/ms
-//Iteration   3: 567.104 ops/ms
-//Iteration   4: 1486.198 ops/ms
-//
-//
-//Result "ch.qos.logback.perf.AsyncWithFileAppenderBenchmark.logbackFile":
-//  794.513 ±(99.9%) 3112.497 ops/ms [Average]
-//  (min, avg, max) = (391.968, 794.513, 1486.198), stdev = 481.662
-//  CI (99.9%): [≈ 0, 3907.010] (assumes normal distribution)
-//
-//
-//Benchmark                                    Mode  Cnt    Score      Error   Units
-//AsyncWithFileAppenderBenchmark.logbackFile  thrpt    4  794.513 ± 3112.497  ops/ms
-
+// Results with 1.3.0-alpha10 (drainTo optimization)
+// 670.666 ±(99.9%) 772.473 ops/ms [Average]
+	
 
 @State(Scope.Benchmark)
 public class AsyncWithFileAppenderBenchmark {
@@ -86,29 +53,28 @@ public class AsyncWithFileAppenderBenchmark {
     Logger log4j2RandomLogger;
     org.slf4j.Logger slf4jLogger;
     org.apache.log4j.Logger log4j1Logger;
-    java.util.logging.Logger julLogger;
-    String outFolder = "";
+    //java.util.logging.Logger julLogger;
     
     
     @Setup
     public void setUp() throws Exception {
-        //System.setProperty("log4j.configurationFile", "log4j2-perf.xml");
+    	
+    	System.setProperty("log4j2.contextSelector", "org.apache.logging.log4j.core.async.AsyncLoggerContextSelector");
+        System.setProperty("log4j.configurationFile",   "log4j2-async-perf.xml");
         System.setProperty("logback.configurationFile", "logback-async-perf.xml");
-        //System.setProperty("log4j.configuration", "log4j12-perf.xml");
+        System.setProperty("log4j.configuration",       "log4j12-async-perf.xml");
 
-        outFolder = System.getProperty("outFolder", "");
         
         deleteLogFiles();
 
-        //log4j2Logger = LogManager.getLogger(AsyncWithFileAppenderBenchmark.class);
-        //log4j2RandomLogger = LogManager.getLogger("TestRandom");
-        slf4jLogger = LoggerFactory.getLogger(AsyncWithFileAppenderBenchmark.class);
-        //log4j1Logger = org.apache.log4j.Logger.getLogger(AsyncWithFileAppenderBenchmark.class);
-        
+        log4j2Logger = LogManager.getLogger(this.getClass());
+        slf4jLogger = LoggerFactory.getLogger(this.getClass());
+        log4j1Logger = org.apache.log4j.Logger.getLogger(this.getClass());
     }
 
     @TearDown
     public void tearDown() {
+    	System.clearProperty("log4j2.contextSelector");
         System.clearProperty("log4j.configurationFile");
         System.clearProperty("log4j.configuration");
         System.clearProperty("logback.configurationFile");
@@ -127,8 +93,8 @@ public class AsyncWithFileAppenderBenchmark {
         //final File log4jRandomFile = new File("target/testRandomlog4j2.log");
         //log4jRandomFile.delete();
         
-        //final File log4j2File = new File("target/testlog4j2.log");
-        //log4j2File.delete();
+        final File log4j2File = new File("target/test-output/log4j2-async.log");
+        log4j2File.delete();
     }
     
     @BenchmarkMode(Mode.Throughput)
@@ -138,4 +104,17 @@ public class AsyncWithFileAppenderBenchmark {
         slf4jLogger.debug(MESSAGE);
     }
 
+    @BenchmarkMode(Mode.Throughput)
+    @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    @Benchmark
+    public void log4j2AsyncFile() {
+    	log4j2Logger.debug(MESSAGE);
+    }
+
+    @BenchmarkMode(Mode.Throughput)
+    @OutputTimeUnit(TimeUnit.SECONDS)
+    @Benchmark
+    public void log4j1File() {
+        log4j1Logger.debug(MESSAGE);
+    }
 }
