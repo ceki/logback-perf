1f9ebb8eeac9817375ca1a932e4e1f66cc681283
Ceki Gulcu <ceki@qos.ch>
2024-01-09 08:12:41 +0100

    add results
    Signed-off-by: Ceki Gulcu <ceki@qos.ch>

--- src/main/java/ch/qos/logback/perf/DisabledLogger.java
+++ src/main/java/ch/qos/logback/perf/DisabledLogger.java
@@ -14,18 +14,22 @@ import org.slf4j.LoggerFactory;
 import java.util.concurrent.TimeUnit;
 
 // HOW TO RUN THIS TEST
-// java -jar logback-perf/target/benchmarks.jar ".*DisabledLogger.*" -f 1 -wi 3 -i 3 -f 1
+// java -jar logback-perf/target/benchmarks.jar ".*DisabledLogger.*" -wi 3 -i 3 -f 3
 
 // Notable results
-// Benchmark                       Mode  Cnt    Score   Error  Units
-// DisabledLogger.direct           avgt    2   18.481          ns/op
-// DisabledLogger.fluent           avgt    2   15.631          ns/op
-// DisabledLogger.fluentLong       avgt    2   15.305          ns/op
-//
-// DisabledLogger.directMixed      avgt    2   98.102          ns/op
-// DisabledLogger.fluentMixed      avgt    2  135.866          ns/op
-// DisabledLogger.fluentLongMixed  avgt    2  160.515          ns/op
 
+// Benchmark                          Mode  Cnt   Score   Error  Units
+// DisabledLogger.directDisabled      avgt   12   1.172 ± 0.049  ns/op
+// DisabledLogger.directEnabled       avgt   12  68.901 ± 1.172  ns/op
+// DisabledLogger.directMixed         avgt   12  71.301 ± 2.194  ns/op
+
+// DisabledLogger.fluentDisabled      avgt   12   0.989 ± 0.030  ns/op
+// DisabledLogger.fluentEnabled       avgt   12  88.098 ± 2.157  ns/op
+// DisabledLogger.fluentMixed         avgt   12  89.583 ± 3.157  ns/op
+
+// DisabledLogger.fluentLongDisabled  avgt   12   0.980 ± 0.012  ns/op
+// DisabledLogger.fluentLongEnabled   avgt   12  95.781 ± 1.540  ns/op
+// DisabledLogger.fluentLongMixed     avgt   12  99.017 ± 2.514  ns/op
 
 @State(Scope.Benchmark)
 public class DisabledLogger {
@@ -50,10 +54,8 @@ public class DisabledLogger {
     @BenchmarkMode(Mode.AverageTime)
     @OutputTimeUnit(TimeUnit.NANOSECONDS)
     @Benchmark
-    public void direct() {
-        for (int i = 0; i < RUN_LENGTH; i++) {
-            logger.debug("jello p0={}, p1={}", PARAMS[0], PARAMS[1]);
-        }
+    public void directDisabled() {
+        logger.debug("jello p0={}, p1={}", PARAMS[0], PARAMS[1]);
     }
 
     @BenchmarkMode(Mode.AverageTime)
@@ -68,18 +70,14 @@ public class DisabledLogger {
     @Benchmark
     public void directMixed() {
         logger.info("jello p0={}, p1={}", PARAMS[0], PARAMS[1]);
-        for (int i = 0; i < RUN_LENGTH; i++) {
-            logger.debug("jello p0={}, p1={}", PARAMS[0], PARAMS[1]);
-        }
+        logger.debug("jello p0={}, p1={}", PARAMS[0], PARAMS[1]);
     }
 
     @BenchmarkMode(Mode.AverageTime)
     @OutputTimeUnit(TimeUnit.NANOSECONDS)
     @Benchmark
-    public void fluent() {
-        for (int i = 0; i < RUN_LENGTH; i++) {
-            logger.atDebug().addArgument(PARAMS[0]).addArgument(PARAMS[1]).setMessage("jello p0={}, p1={}").log();
-        }
+    public void fluentDisabled() {
+        logger.atDebug().addArgument(PARAMS[0]).addArgument(PARAMS[1]).setMessage("jello p0={}, p1={}").log();
     }
 
     @BenchmarkMode(Mode.AverageTime)
@@ -94,40 +92,33 @@ public class DisabledLogger {
     @Benchmark
     public void fluentMixed() {
         logger.atInfo().addArgument(PARAMS[0]).addArgument(PARAMS[1]).setMessage("jello p0={}, p1={}").log();
-        for (int i = 0; i < RUN_LENGTH; i++) {
-            logger.atDebug().addArgument(PARAMS[0]).addArgument(PARAMS[1]).setMessage("jello p0={}, p1={}").log();
-        }
+        logger.atDebug().addArgument(PARAMS[0]).addArgument(PARAMS[1]).setMessage("jello p0={}, p1={}").log();
     }
-
 
     @BenchmarkMode(Mode.AverageTime)
     @OutputTimeUnit(TimeUnit.NANOSECONDS)
     @Benchmark
-    public void fluentLong() {
-        for (int i = 0; i < RUN_LENGTH; i++) {
-            logger.atDebug().addArgument(PARAMS[0]).addArgument(PARAMS[1]).addKeyValue("a", "a").addKeyValue("b", "b")
-                    .setMessage("jello p0={}, p1={}").log();
-        }
+    public void fluentLongDisabled() {
+        logger.atDebug().addArgument(PARAMS[0]).addArgument(PARAMS[1]).addKeyValue("a", "a").addKeyValue("b", "b").setMessage("jello p0={}, p1={}")
+                .log();
     }
 
     @BenchmarkMode(Mode.AverageTime)
     @OutputTimeUnit(TimeUnit.NANOSECONDS)
     @Benchmark
     public void fluentLongEnabled() {
-        logger.atInfo().addArgument(PARAMS[0]).addArgument(PARAMS[1]).addKeyValue("a", "a").addKeyValue("b", "b")
-                .setMessage("jello p0={}, p1={}").log();
+        logger.atInfo().addArgument(PARAMS[0]).addArgument(PARAMS[1]).addKeyValue("a", "a").addKeyValue("b", "b").setMessage("jello p0={}, p1={}")
+                .log();
     }
 
     @BenchmarkMode(Mode.AverageTime)
     @OutputTimeUnit(TimeUnit.NANOSECONDS)
     @Benchmark
     public void fluentLongMixed() {
-        logger.atInfo().addArgument(PARAMS[0]).addArgument(PARAMS[1]).addKeyValue("a", "a").addKeyValue("b", "b")
-                .setMessage("jello p0={}, p1={}").log();
-        for (int i = 0; i < RUN_LENGTH; i++) {
-            logger.atDebug().addArgument(PARAMS[0]).addArgument(PARAMS[1]).addKeyValue("a", "a").addKeyValue("b", "b")
-                    .setMessage("jello p0={}, p1={}").log();
-        }
+        logger.atInfo().addArgument(PARAMS[0]).addArgument(PARAMS[1]).addKeyValue("a", "a").addKeyValue("b", "b").setMessage("jello p0={}, p1={}")
+                .log();
+        logger.atDebug().addArgument(PARAMS[0]).addArgument(PARAMS[1]).addKeyValue("a", "a").addKeyValue("b", "b").setMessage("jello p0={}, p1={}")
+                .log();
     }
 }
 
